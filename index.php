9e12a03698af0cdfc6e087e2fe72663c87e5d50e
Cristiam Diaz <c.diaz@udla.edu.co>
2014-04-15 02:23:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,106 +1,117 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Dashboard - Flisol 2014</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-<link href="assets/css/pages/signin.css" rel="stylesheet" type="text/css">
-<link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
-        rel="stylesheet">
-<link href="assets/css/font-awesome.css" rel="stylesheet">
-<link href="assets/css/style.css" rel="stylesheet">
-<link href="assets/css/pages/dashboard.css" rel="stylesheet">
-<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-</head>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=632493393434839";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>      
-<body>
+<?php 
+include "include/header.php";
+?>
 
-
-<div class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
-    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
-                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">Flisol 2014 </a>
-      <div class="nav-collapse">
-        <!-- The drop down menu -->
-        <ul class="nav pull-right">
-
-<li class="dropdown" id="menuLogin">
-            <a href="#" id="navLogin"><i class="icon-user"></i> Acceder</a>
-            <div id="loginform"class="dropdown-menu" style="padding:17px;">
-             <form action="#" method="post">
-    
-      <h2>Acceso</h2>   
-      
+<div class="main">
+  <div class="main-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span5">
+          <div class="widget widget-nopad">
+            <div class="widget-header"> <i class="icon-list-alt"></i>
+              <h3> Estadisticas</h3>
+            </div>
+            <!-- /widget-header -->
+            <div class="widget-content">
+              <div class="widget big-stats-container">
+                <div class="widget-content">
+                  <h6 class="bigstats">A fully responsive premium quality admin template built on Twitter Bootstrap by <a href="http://www.egrappler.com" target="_blank">EGrappler.com</a>.  These are some dummy lines to fill the area.</h6>
+                  <div id="big_stats" class="cf">
+                    <div class="stat"> <i class="icon-anchor"></i> <span class="value">851</span> </div>
+                    <!-- .stat -->
+                    
+                    <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value">423</span> </div>
+                    <!-- .stat -->
+                    
+                    <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value">922</span> </div>
+                    <!-- .stat -->
+                    
+                    <div class="stat"> <i class="icon-bullhorn"></i> <span class="value">25%</span> </div>
+                    <!-- .stat --> 
+                  </div>
+                </div>
+                <!-- /widget-content --> 
+                
+              </div>
+            </div>
+          </div>
+         
+          
+        </div>
+        <!-- /span6 -->
+        <div class="span4">
+          <div class="widget">
+            <div class="widget-header"> <i class="icon-bookmark"></i>
+              <h3>Consulte el estado de su equipo</h3>
+            </div>
+            <!-- /widget-header -->
+            <div class="widget-content">
+  
+     <form action="#" method="post">
+          
       <div class="login-fields">
         
-        <p>Porfavor suministre sus credenciales de acceso</p>
+        <p>Digite el numero de su documento de identidad</p>
         
         <div class="field">
-          <input type="text" id="username" name="username" value="" placeholder="Usuario" class="login username-field" />
+          <input type="text" id="identificacion" name="identificacion" value="" placeholder="Documento de Identidad" class="login username-field" />
         </div> <!-- /field -->
-        
-        <div class="field">
-          <input type="password" id="password" name="password" value="" placeholder="Contraseña" class="login password-field"/>
-        </div> <!-- /password -->
         
       </div> <!-- /login-fields -->
       
       <div class="login-actions">
                   
-        <button class="button btn btn-success btn-large">Acceder</button>
+        <button class="button btn btn-warning btn-large">Consultar</button>
         
       </div> <!-- .actions -->
       
       
       
-    </form>
+    </form>         </div>
+            <!-- /widget-content --> 
+          </div>
+          <!-- /widget -->
+         
+        </div>
+        <!-- /span6 -->
+
+
+        <div class="span3">
+          <div class="widget">
+            <div class="widget-header"> <i class="icon-bookmark"></i>
+              <h3>Siguenos en Facebook</h3>
             </div>
-          </li>
-        </ul>
+            <!-- /widget-header -->
+            <div class="widget-content">
+     
+     <div class="fb-like-box" data-href="https://www.facebook.com/flisolflorencia" data-width="255" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+<div id="fb-root"></div>
+    </div>
+          <!-- /widget -->
+         </div>
+        </div>
+
 
       </div>
-      <!--/.nav-collapse --> 
+      <!-- /row --> 
     </div>
     <!-- /container --> 
   </div>
-  <!-- /navbar-inner --> 
+  <!-- /main-inner --> 
 </div>
-<!-- /navbar --> 
-<div class="subnavbar">
-  <!-- <div class="subnavbar-inner">
+<!-- /main -->
+<div class="extra">
+  <div class="extra-inner">
     <div class="container">
-      <ul class="mainnav">
-        <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
-        <li><a href="reports.html"><i class="icon-list-alt"></i><span>Reports</span> </a> </li>
-        <li><a href="guidely.html"><i class="icon-facetime-video"></i><span>App Tour</span> </a></li>
-        <li><a href="charts.html"><i class="icon-bar-chart"></i><span>Charts</span> </a> </li>
-        <li><a href="shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
-        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="icons.html">Icons</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="pricing.html">Pricing Plans</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="signup.html">Signup</a></li>
-            <li><a href="error.html">404</a></li>
-          </ul>
-        </li>
-      </ul>
+      <div class="row">
+                   
+      <!-- /row --> 
     </div>
-    <!-- /container  
-  </div> 
-  <!-- /subnavbar-inner --> 
+    <!-- /container --> 
+  </div>
+  <!-- /extra-inner --> 
 </div>
-<!-- /subnavbar -->
+<!-- /extra -->
+<?php 
+include "include/footer.php";
+?>
